c433994fda81045cae957d13fe0314da62fda547
Clemence Chopin Altima <telynor@gmail.com>
2025-11-20 16:52:10 +0100

    WIP: Ajout du file_manager.py
    - Ajout sanitize_path
    - Ajout resolve_file_path
    - Ajout file_exists

--- 05_web_security/http-web-server/file_manager.py
+++ 05_web_security/http-web-server/file_manager.py
@@ -1,2 +1,30 @@
 #!/usr/bin/python3
+import string
+import glob
+
+
+def sanitize_path(requested_path):
+    return requested_path.replace("..", "").replace("/", "")
+
+
+def resolve_file_path(path):
+    if len(path) == 0 or path == "/":
+        return "index.html"
+    else:
+        return sanitize_path(path)
+
+
+def file_exists(file_path):
+    if len(glob.glob(f"public/{resolve_file_path(file_path)}")) > 0:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    print(resolve_file_path("../../hello"))
+    print(file_exists("test.html"))
+    print(file_exists("../test.html"))
+    print(file_exists("public/testdd.html"))
+
 
